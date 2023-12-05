ae594dd82e58e675a0e6697fe8ddbc46a9e60b40
penguinify <103973145+Penguinify@users.noreply.github.com>
2023-12-05 15:45:53 -0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -8,18 +8,14 @@ inject_button.addEventListener("click", () => {
     let win = window.open();
     let script = win.document.body.createElement("script");
     script.innerHTML = js;
-    console.log(script);
-    win.document.addEventListener("DOMContentLoaded", () => {
-        setTimeout(() => {
-            win.document.body.appendChild(script);
-        }, 1000);
-    })
+    win.document.body.appendChild(script);
+
 })
 
 let inject_car_axle_client = document.getElementById("inject-car-axle-client");
 
 inject_car_axle_client.addEventListener("click", () => {
-    js_input.value = `fetch("https://raw.githubusercontent.com/car-axle-client/car-axle-client/v5/docs/testing/index.js")
+    js_input.value = `fetch("https://raw.githubusercontent.com/car-axle-client/car-axle-client/v6/docs/testing/index.js")
                 .then((res) => res.text())
                 .then((js) => eval(js));`;
 
@@ -29,6 +25,5 @@ inject_car_axle_client.addEventListener("click", () => {
     let win = window.open();
     let script = win.document.createElement("script");
     script.innerHTML = js;
-    console.log(js);
     win.document.body.appendChild(script);
 })
