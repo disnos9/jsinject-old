3568642511d00bcfae9f0144424f6381ee0cfc3a
Dino <github@dinois.fun>
2023-12-20 14:11:53 -0500

    add scroll too top

--- static/settings.js
+++ static/settings.js
@@ -1,21 +1,34 @@
-document.getElementById('settings-icon').addEventListener('click', function() {
-    document.getElementById('settings-popup').style.display = 'block';
+var settingsBackdrop = document.getElementById('settings-backdrop');
+  var settingsPopup = document.getElementById('settings-popup');
+
+  document.getElementById('settings-icon').addEventListener('click', function() {
+    settingsBackdrop.style.display = 'block';
+    settingsPopup.style.display = 'block';
+    settingsPopup.style.filter = 'blur(5px)';
   });
 
-  document.getElementById('closeSettings').addEventListener('click', function() {
-    document.getElementById('settings-popup').style.display = 'none';
-  });
+  document.getElementById('closeSettings').addEventListener('click', closeSettingsPopup);
 
   document.getElementById('saveSettings').addEventListener('click', function() {
-    // Save settings to cookies
     saveSetting('closeOnInjection', document.getElementById('closeOnInjection').checked);
     saveSetting('choice2', document.getElementById('choice2').checked);
-
-    // Close the settings popup
-    document.getElementById('settings-popup').style.display = 'none';
+    closeSettingsPopup();
   });
 
-  // Function to save settings to cookies
+  function closeSettingsPopup() {
+    settingsBackdrop.style.display = 'none';
+    settingsPopup.style.display = 'none';
+    settingsPopup.style.filter = 'blur(0)';
+  }
+
   function saveSetting(name, value) {
     document.cookie = name + '=' + value + '; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/';
+  }
+
+
+
+// scrolls to top
+
+  function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
